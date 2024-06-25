090646935564ba4e2c534d16e4c38314ac27d442
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-06-25 17:21:54 +0300

    Add more solutions

---
+++ JS Front-End/JS Front-End - Exercises/1.1 JS Syntax Fundamentals – Exercises/04_print_and_sum.js
@@ -0,0 +1,18 @@
+function printAndSum (num1, num2) {
+    let result = 0;
+    let numbers = []
+
+    for (let i = num1; i <= num2; i++) {
+        result += i
+        numbers.push(i)
+       
+    }
+    console.log(numbers.join(' '));
+    console.log(`Sum: ${result}`);
+}
+
+
+//Test case
+printAndSum(5, 10)
+// printAndSum(0, 26)
+// printAndSum(50, 60)
