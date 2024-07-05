2614a6d98b3b8e22525becf080f15311341db0e3
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-07-05 17:41:11 +0300

    add more exercises

---
+++ JS Front-End/JS Front-End - Exercises/3. Functions and Statements - More Exercise/01_car_wash.js
@@ -0,0 +1,22 @@
+function carWash(commandsArray) {
+    let carCleaned = 0
+
+    const commands = {
+        'soap': (num) => num + 10,
+        'water': (num) => num * 1.20,
+        'vacuum cleaner': (num) => num * 1.25,
+        'mud': (num) => num * 0.90
+    }
+
+    for (let command of commandsArray) {
+        carCleaned += commands[command](carCleaned)
+    }
+
+    console.log(carCleaned);
+}
+
+
+
+//Test code
+
+carWash(['soap', 'soap', 'vacuum cleaner', 'mud', 'soap', 'water'])
