ae75b064b19a81b5bc63a77324600aa3dd398780
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-07-22 11:41:40 +0300

    Add another exercises

---
+++ JS Front-End/JS Front-End - Exercises/4. Objects and Classes - Exercises/02-towns.js
@@ -0,0 +1,29 @@
+function townInfo(strArray) {
+    strArray.forEach(element => {
+        const [town, latitude, longitude] = element.split(' | ');
+
+        const formattedLatitude = parseFloat(latitude).toFixed(2);
+        const formattedLongitude = parseFloat(longitude).toFixed(2);
+
+        const townObject = {
+            town: town,
+            latitude: formattedLatitude,
+            longitude: formattedLongitude
+
+        }
+
+        console.log(townObject);
+
+    });
+}
+
+
+//Test case:
+// townInfo(
+//     [
+//         'Sofia | 42.696552 | 23.32601',
+//         'Beijing | 39.913818 | 116.363625'
+//     ]
+// )
+
+townInfo(['Plovdiv | 136.45 | 812.575'])
