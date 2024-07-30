73e9aa62814d1fb5badce6ef2c761d4b64056fd7
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-07-30 14:49:38 +0300

    add more exercises

--- JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
+++ JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,10 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const numberOneElement = document.getElementById('num1');
+    const numberTwoElement = document.getElementById('num2');
+    const resultElement = document.getElementById('sum');
+
+    const numberOne = Number(numberOneElement.value);
+    const numberTwo = Number(numberTwoElement.value);
+
+    resultElement.value = numberOne + numberTwo;
 }
