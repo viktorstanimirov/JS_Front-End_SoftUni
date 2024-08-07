ddcfc2ca0a7c9b42cfe74648d741198899dd9cc8
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-08-07 16:11:48 +0300

    add more exercises

--- JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/04. Search in List/search.js
+++ JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   const searchValueEl = document.getElementById('searchText').value;
+   const listTownElements = Array.from(document.querySelectorAll('#towns li'));
+
+   let matchCount = 0;
+   console.log(searchValueEl);
+
+
+   for (const element of listTownElements) {
+      if (element.textContent.includes(searchValueEl)) {
+         matchCount += 1;
+         element.style.fontWeight = 'bold';
+         element.style.textDecoration = 'underline';
+      }
+   }
+   let resultEl = document.getElementById('result');
+   resultEl.textContent = `${matchCount} matches found`
 }
--- JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,21 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const searchedValueEl = document.getElementById('searchField');
+      const allElements = Array.from(document.querySelectorAll('td'));
+      const allRowsElements = Array.from(document.querySelectorAll('tr'));
 
+      for (let row of allRowsElements) {
+         row.classList.remove('select')
+      }
+
+      for (const element of allElements) {
+         if (element.textContent.includes(searchedValueEl.value)) {
+            element.parentElement.className = 'select'
+         }
+
+      }
+      searchedValueEl.value = '';
    }
+
 }
