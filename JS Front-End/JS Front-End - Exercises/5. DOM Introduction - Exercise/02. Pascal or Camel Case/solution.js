0d53565a1500f8a0bb3a03e19209835a7c872b6d
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-08-07 14:14:48 +0300

    add more solutions

--- JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+    const validNamingConvention = ['Camel Case', 'Pascal Case'];
+
+    const [textEl, namingConventionEl] = document.querySelectorAll('input[type="text"]');
+    const resultEl = document.getElementById('result');
+
+    if (!validNamingConvention.includes(namingConventionEl.value)) {
+        resultEl.textContent = "Error!";
+    } else {
+        let result;
+        if (namingConventionEl.value === 'Camel Case') {
+            result = textEl.value.toLowerCase()
+                .split(' ')
+                .map((word, index) => {
+                    if (index === 0) {
+                        return word;
+                    }
+                    return word.charAt(0).toUpperCase() + word.slice(1);
+                })
+                .join('');
+        } else if (namingConventionEl.value === 'Pascal Case') {
+            result = textEl.value.toLowerCase()
+                .split(' ')
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                .join('');
+        }
+        resultEl.textContent = result;
+    }
 }
--- JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const buttonEl = document.getElementsByClassName('button')[0];
+    const extraContentEl = document.getElementById('extra');
+
+    if (extraContentEl.style.display === 'none') {
+        extraContentEl.style.display = 'block';
+        buttonEl.textContent = 'Less'
+    } else {
+        extraContentEl.style.display = 'none';
+        buttonEl.textContent = 'More'
+    }
 }
