a39f851a9ad5fc4bc36d3093d68e0a0581f9fd6e
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-08-09 14:41:01 +0300

    added more exercises

--- JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,16 @@
 function generateReport() {
-    //TODO
+    const columnsElements = Array.from(document.querySelectorAll('th input'));
+    const selectedColumns = columnsElements.map((input, index) => input.checked ? index : -1).filter(index => index !== -1);
+
+    const reportList = [];
+
+    document.querySelectorAll('tbody tr').forEach(row => {
+        const reportItem = {};
+        selectedColumns.forEach(colIndex => {
+            const headerName = columnsElements[colIndex].name;
+            reportItem[headerName] = row.cells[colIndex].textContent.trim();
+        });
+        reportList.push(reportItem);
+    });
+    document.getElementById('output').value = JSON.stringify(reportList, null, 2);
 }
