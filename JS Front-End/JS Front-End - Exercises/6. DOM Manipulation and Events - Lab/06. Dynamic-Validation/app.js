b61ffff2125596c723005276a12f1e6df1157b62
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-08-12 16:42:47 +0300

    added more solutions

--- JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    let inputFieldEl = document.getElementById('email');
+    inputFieldEl.addEventListener('change', onChangeEvent)
+
+    let searchedPattern = /[a-z]+@[a-z]+\.[a-z]+/g
+
+    function onChangeEvent(el) {
+        let currentValue = el.currentTarget.value;
+        if (!currentValue.match(searchedPattern)) {
+            el.currentTarget.className = 'error'
+        } else {
+            el.currentTarget.classList.remove('error')
+
+        }
+    }
 }
--- JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,42 @@
 function solve() {
-   //TODO...
+    const resultElement = document.querySelector('textarea[disabled]');
+    const checkoutButton = document.querySelector('button.checkout');
+
+    const productCatalogElement = document.querySelector('.shopping-cart');
+
+    let allProducts = [];
+
+    productCatalogElement.addEventListener('click', (e) => {
+        if (e.target.tagName !== 'BUTTON') {
+            return;
+        };
+
+        if (e.target.textContent.trim() !== 'Add') {
+            return;
+        };
+
+        const productEl = e.target.closest('.product');
+        const productName = productEl.querySelector('.product-title').textContent;
+        const productPrice = Number(productEl.querySelector('.product-line-price').textContent);
+
+        resultElement.value += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`;
+
+        if (!allProducts.hasOwnProperty(productName)) {
+            allProducts[productName] = productPrice
+
+        } else {
+            allProducts[productName] += productPrice
+        };
+    });
+
+    checkoutButton.addEventListener('click', () => {
+        const totalProductsSum = Object.values(allProducts).reduce((acc, value) => acc + value, 0);
+        const productNames = Object.keys(allProducts).join(', ');
+
+        resultElement.value += `You bought ${productNames} for ${totalProductsSum.toFixed(2)}.\n`;
+
+        document.querySelectorAll('button')
+            .forEach(el => el.setAttribute('disabled', 'disabled'));
+    });
+
 }
