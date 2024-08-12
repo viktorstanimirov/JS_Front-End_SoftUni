ef97d155329bece8f01533a98fe57c576bc01263
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-08-12 10:58:59 +0300

    add more exercises

--- JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,14 @@
 function addItem() {
-    //TODO...
+    const input = document.getElementById('newItemText');
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    const deleteLink = document.createElement('a');
+    deleteLink.textContent = '[Delete]';
+    deleteLink.href = '#';
+    deleteLink.addEventListener('click', (e) => {
+        e.target.parentNode.remove();
+    });
+    li.appendChild(deleteLink);
+    document.getElementById('items').appendChild(li);
+    input.value = '';
 }
