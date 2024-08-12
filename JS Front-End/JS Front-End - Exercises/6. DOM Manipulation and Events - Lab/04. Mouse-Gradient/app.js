339dca43eb33a9e6f4b958bce146f63bd5f00acb
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-08-12 14:55:51 +0300

    add more solutions

--- JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
+++ JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const resultElement = document.getElementById('result');
+    const gradientElement = document.getElementById('gradient');
+
+    gradientElement.addEventListener('mousemove', (e) => {
+        const currentPosition = e.offsetX;
+        const elementWidth = e.currentTarget.clientWidth
+
+        const percent = Math.floor((currentPosition / elementWidth) * 100);
+
+        resultElement.textContent = `${percent}%`;
+    });
 }
--- JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    let allInputs = Array.from(document.querySelectorAll('div input'))
+
+    for (let inputBox of allInputs) {
+        inputBox.addEventListener('focus', onFocus)
+        inputBox.addEventListener('blur', outOfFocus)
+    }
+
+    function onFocus(e) {
+        e.currentTarget.parentNode.className = 'focused'
+    }
+
+    function outOfFocus(e) {
+        e.currentTarget.parentNode.classList.remove('focused')
+    }
+
 }
